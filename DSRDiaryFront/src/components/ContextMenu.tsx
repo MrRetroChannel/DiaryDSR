f862604567b63701ef358e8482f976049be641c5
MrRetroChannel <69257256+MrRetroChannel@users.noreply.github.com>
2023-08-14 21:15:25 +0300

    Update ContextMenu.tsx

--- DSRDiaryFront/src/components/ContextMenu.tsx
+++ DSRDiaryFront/src/components/ContextMenu.tsx
@@ -7,7 +7,7 @@ export default function ContextMenu({x, y, close, task}: {x: number, y: number, 
     const { setTasks } = useContext(TaskContext);
     const { completedTasks, setCompletedTasks } = useContext(CompletedTaskContext);
     return (
-        <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0}} onClick={close}>
+        <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0, zIndex: 1001}} onClick={close}>
             <div className="contextMenu" style={{top: y, left: x}}>
                 {task.status == Status.INPROGRESS && <div className="completeCtxButton" onClick={ async () => { task.status = Status.DONE; await post(`api/Tasks/setComplete`, JSON.stringify(task)); setCompletedTasks!([...completedTasks!, task])}}>Выполнить</div> }
                 <div className="deleteCtxButton" onClick={ async () => { setTasks!.remove(task.taskid); await apiDelete(`api/Tasks`, task!.taskid); close(); }}>Удалить</div>
